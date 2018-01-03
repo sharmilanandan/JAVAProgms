14c6456b649f1e124135a37e8d4726c4bec8e064
sharmilanandan <34845267+sharmilanandan@users.noreply.github.com>
2018-01-03 20:39:27 +0530

    Initial commit

---
+++ src/programs/Patt1.java
@@ -0,0 +1,19 @@
+package programs;
+
+public class Patt1 {
+	public static void main(String[] args) {
+		int val=1,n=4;
+			for(int i=1;i<=n;i++)
+			{
+				for(int j=1;j<=i;j++)
+					System.out.print(val--);
+				System.out.println();
+				if(i!=4)
+				{val++;
+				 val=val+val*2;
+				}
+			
+		}
+	}
+
+}
